86c520ec5df651705a5c0d493dde5b5febb2bbd1
Jonathan.Cohen <jcohen66g@gmail.com>
2021-02-01 12:38:48 -0500

    Refactored majority element indexes problem

--- questions/easy/majority_element_indexes.py
+++ questions/easy/majority_element_indexes.py
@@ -1,5 +1,5 @@
-from math import floor
-from collections import defaultdict
+
+from collections import Counter
 
 def majority_element_indexes(lst):
     '''
@@ -11,28 +11,37 @@ def majority_element_indexes(lst):
     [0, 1]
     >>> majority_element_indexes([1,1,2,2,2,2])
     [2, 3, 4, 5]
+    >>> majority_element_indexes([4,4,4,1,1,2,2,2,2,4,4])
+    []
+    >>> majority_element_indexes([6,6,6,1,1,2,6,6,6,])
+    [0, 1, 2, 6, 7, 8]
     >>> majority_element_indexes([1,2])
     []
     >>> majority_element_indexes([])
     []
     '''
-    # no me due to lack of elements
-    if len(lst) <= 2:
+    # Nothing to do
+    if len(lst) == 0:
         return []
-    # Calculate me
-    elems = defaultdict(lambda: 0)
-    for x in lst:
-        elems[x] += 1
-    me = max(elems, key=elems.get)
 
-    # validate me against floor
-    flr = floor(len(lst)/2)
-    result = []
-    if elems[me] <= flr:
-        return result
+    # Build dictionary with corresponding counts
+    count  = Counter(lst)
+    # Sort them in reverse order so highest is first
+    # Sorted takes O(nlogn) to sort
+    top_elems = sorted(
+        count.keys(),
+        key=lambda x: -count[x]
+    )
+    # Grab the key with highest count which is now at front of list
+    maj_elem = top_elems[0]
 
-    # Populate the result with the index positions
-    for i in range(len(lst)):
-        if me == lst[i]:
-            result.append(i)
-    return result
+    # there exists a majority element
+    if count[maj_elem] > len(lst) // 2:
+        return [
+            # List comprehension guarding just the majority elements.
+            # List comprehension takes O(n)
+            i for i, elem in enumerate(lst) if elem == maj_elem
+        ]
+    else:
+        # Violates number of maj_elems rule so return empty list.
+        return []
