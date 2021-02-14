ab8154ada9ee994bd245e417c0ac3b637be11321
Jonathan T. Cohen <jcohen66@optonline.net>
2021-02-14 16:41:21 -0500

    Added expression tree problem

---
+++ binary_trees/expression_tree.py
@@ -0,0 +1,59 @@
+class Expr(object):
+    def __init__(self, name):
+        self.name = name
+
+class Times(Expr):
+    def __init__(self, l, r):
+        self.l = l
+        self.r = r
+
+    def __str__(self):
+        return "(" + str(self.l) + "*" + str(self.r) + ")"
+
+    def eval(self, env):
+        return self.l.eval(env) * self.r.eval(env)
+
+class Plus(Expr):
+    def __init__(self, l, r):
+        self.l = l
+        self.r = r
+
+    def __str__(self):
+        return "(" + str(self.l) + "+" + str(self.r) + ")"
+
+    def eval(self, env):
+        return self.l.eval(env) + self.r.eval(env)
+
+class Const(Expr):
+    def __init__(self, val):
+        self.val = val
+
+    def __str__(self):
+        return str(self.val)
+
+    def eval(self, env):
+        return self.val
+
+class Var(Expr):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+    def eval(self, env):
+        return env[self.name]
+
+e1 = Times(Const(3), Plus(Var("y"), Var("x")))
+
+e2 = Plus(Times(Const(3), Var("y")), Var("x"))
+
+env = {"x": 2, "y": 4}
+
+
+
+
+print(e1)
+print(e1.eval(env))
+print(e2)
+print(e2.eval(env))
